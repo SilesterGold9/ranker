21b40964de58187c71b7c1c6e055614559cb4735
SilesterGold9 <silvestredourado18@gmail.com>
2024-03-14 03:02:39 +0100

    Estilizando, finalmente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,35 @@
 import { NavLink } from "react-router-dom";
 import { routes } from "./routes";
+import styles from "./Header.module.css";
 
 const Header = () => {
 	return (
-		<nav>
-			<ul>
-				{routes.map((route, index) => (
-					<li key={index}>
-						<NavLink
-							to={route.path}
-							end={route.exact}
-							className={({ isActive }) =>
-								isActive ? "active" : undefined
-							}
-						>
-							{route.name}
-						</NavLink>
-					</li>
-				))}
-			</ul>
-		</nav>
+		<header>
+			{/* Logo com link para a página inicial */}
+			<NavLink to='/' className={styles.logoLink}>
+				<img
+					src='src\assets\images\ranker-high-resolution-logo-transparent.svg'
+					alt='logo'
+					className={styles.logo}
+				/>
+			</NavLink>
+			<nav className={styles.nav}>
+				<ul>
+					{routes.map((route, index) => (
+						<li key={index}>
+							<NavLink
+								to={route.path}
+								end={route.exact}
+								className={({ isActive }) =>
+									isActive ? styles.active : undefined
+								}>
+								{route.name}
+							</NavLink>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</header>
 	);
 };
 
